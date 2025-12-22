800711278a2eba0e03983f079ab4cd93ad5efc83
Munkhjargal Manlaibayar <24b1num0285@stud.num.edu.mn>
2025-12-22 15:57:52 +0800

    Lab13 changes

--- C/Lab13/1_cw.c
+++ C/Lab13/1_cw.c
@@ -3,13 +3,13 @@
 #include<limits.h>
 int max_integers(int count, ...) { //huvisah argument
     if(count <=0){
-        return INT_MIN; //hamgiin baga utgiig butsaana
+        return INT_MIN; //hamgiin baga utgiig butsaana -265354675976086768795765
     }
     va_list args;
     va_start(args, count); //3 ruu ehleed zaana
     int max = va_arg(args, int); //ehni ugta, int turliin utga unshina
     for(int i=1;i<count;i++){
-        int current = va_arg(args, int);
+        int current = va_arg(args, int); //odo bga toog unshaad daraginhruu shiljene
         if(current > max){
             max = current;
         }
