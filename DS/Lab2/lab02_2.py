413398dde5e4c5d26e70d64a5e4b6c481bfede42
Munkhjargal Manlaibayar <24b1num0285@stud.num.edu.mn>
2026-03-01 23:39:25 +0800

    ds/lab2/1,2,3

--- DS/Lab2/lab02_2.py
+++ DS/Lab2/lab02_2.py
@@ -4,25 +4,40 @@ students = {
 }
 def add_student(student_id, name, age, major):
     # Оюутны мэдээллийг нэмэх
-    students[student_id]={
-    "name" : name,
-    "age" : age,
-    "major" : major
-    }
+    students[student_id]=(name, age, major)
 
-
-add_student("S003", "uyanga", 22, "CS")
-# print(students)
 def find_student_by_id(student_id):
     # Оюутны мэдээллийг ID-аар хайх
-    return students.get(student_id)
-id = input("Enter student id: ")
-if student_id in students:
+    if student_id in students:
+        student = students[student_id]
+        print(f"Student Found: {student}")
+        return student
+    else:
+        print("Student not found")
 
+def list_students_by_major(major):
+    # Тодорхой хөтөлбөрийн бүх оюутнуудыг жагсаах
+    major_students = []
+    for  student_id, student_info in students.items():
+        name, age, student_major = student_info
+        if student_major == major:
+            major_students.append(student_info)
+    if major_students:
+        print(f"'{major} ' major students")
+        for student in major_students:
+            print(f" {student}")
+    else:
+        print(f"'{major}' major doesn't have students")
+    return major_students
+    
+def count_students():
+    # Нийт оюутны тоог тоолох
+    count = len(students)
+    print(f"Number of students: {count}")
+    return count
 
-# def list_students_by_major(major):
-#     # Тодорхой хөтөлбөрийн бүх оюутнуудыг жагсаах
+add_student("S003", "Уянга", 22, "Компьютерын ухаан")
+find_student_by_id("S002") # Гаралт: ("Болд", 21, "Программ хангамж")
+list_students_by_major("Компьютерын ухаан") # Гаралт: [("Бат", 20, "Компьютерын ухаан), ("Уянга", 22, "Компьютерын ухаан")]
+count_students() # Гаралт: 3
 
-# def count_students():
-#     # Нийт оюутны тоог тоолох
-#     return len(students)
--- DS/Lab2/lab02_3.py
+++ DS/Lab2/lab02_3.py
@@ -1,3 +1,22 @@
-sentence = "the cat and the dog and the bird"
-count = len(sentence.split())
-print(f"number of words: ", count)
+# 1. Өгүүлбэр оруулах
+sentence =  "the cat and the dog and the bird"
+
+# 2. Үг бүр хэдэн удаа давтагдсаныг тоолох
+words = sentence.split()
+word_count = {}
+for i in words:
+    i = i.lower()
+    if i in word_count:
+        word_count[i] += 1
+    else:
+        word_count[i] = 1
+
+# 3. Үр дүнг dictionary-д хадгалах
+
+
+
+# 4. Нэгээс олон удаа гарсан үгсийг хэвлэх
+print("Duplicate words: ")
+for i, count in word_count.items():
+    if count > 1:
+        print(f"{i}: {count}")
